b21b40471c5d71f91a752f1d2e87131381cb00f0
harikas25 <31838010+harikas25@users.noreply.github.com>
2017-09-12 11:44:54 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+        student s =new student(14,"harika sanga",1997-06-13,150);
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
